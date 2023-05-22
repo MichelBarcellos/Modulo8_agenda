5911c4dcd16719600addc971168510eb2d91e128
MichelBarcellos <124924349+MichelBarcellos@users.noreply.github.com>
2023-05-22 18:47:06 -0300

    Modulo_8_agenda
    agenda

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("formAtividade");
+
+let linhas = " ";
+const nomeContato = [];
+const numeroContato = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function atualizaTabela() {
+    const inputNovoName = document.getElementById('NomeContato');
+    const inputNovoNumber = document.getElementById('NumeroContato');
+
+    if (nomeContato.includes(inputNovoName.value)) {
+        alert(`Este contato ja foi inserido ${inputNovoName.value} já foi inserida!`)
+
+
+    } else {
+        nomeContato.push(inputNovoName.value);
+        numeroContato.push(parseInt(inputNovoNumber.value));
+
+
+        let linha = '<tr>';
+        linha += `<td>${inputNovoName.value} </td> `;
+        linha += `<td>${inputNovoNumber.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+}
+function adicionaLinha() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
